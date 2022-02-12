c51fbad3e7b98cb444d7e937f8fbb75916e48802
David Park <daviidpaark@gmail.com>
2022-02-11 21:19:07 -0500

    Homework 1 zip

--- A00/client.py
+++ A00/client.py
@@ -16,37 +16,39 @@ class Query:
         convert all query data into network ready bytes
         :return: bytes of query data
         """
-        type_bytes = bytearray(str.encode(self.q_type, 'utf-8'))
-        length_bytes = bytearray(self.length.to_bytes(1, 'big'))
-        msg_bytes = bytearray(str.encode(self.msg, 'utf-8'))
+        type_bytes = bytearray(str.encode(self.q_type, "utf-8"))
+        length_bytes = bytearray(self.length.to_bytes(1, "big"))
+        msg_bytes = bytearray(str.encode(self.msg, "utf-8"))
         return type_bytes + length_bytes + msg_bytes
 
 
 def main():
-    msg = input('Email to ask\n')
+    msg = input("Email to ask\n")
     # Create a TCP/IP socket
-    sock = socket.create_connection(('127.0.0.1', 3100))
+    sock = socket.create_connection(("127.0.0.1", 3100))
     try:
         # create query from input
-        query = Query('Q', len(msg), msg)
+        query = Query("Q", len(msg), msg)
         # send query data to server
         sock.sendall(query.get_data())
         while True:
             # read data send by server
             # read query type
             r_type_data = sock.recv(1)
-            r_type = r_type_data.decode('utf-8')
-            if r_type == 'R':
+            r_type = r_type_data.decode("utf-8")
+            if r_type == "R":
                 # read msg length
                 msg_length_data = sock.recv(1)
                 # decode the msg_length with Big Endian (network byte order)
-                msg_length = int.from_bytes(msg_length_data, 'big')
+                msg_length = int.from_bytes(msg_length_data, "big")
                 # read the actual msg
                 msg_data = sock.recv(msg_length)
-                msg = msg_data.decode('utf-8')
-                print(f'------------Response------------\n'
-                      f'Type: {r_type}\nLength: {msg_length}\nAnswer: {msg}\n'
-                      f'--------------------------------')
+                msg = msg_data.decode("utf-8")
+                print(
+                    f"------------Response------------\n"
+                    f"Type: {r_type}\nLength: {msg_length}\nAnswer: {msg}\n"
+                    f"--------------------------------"
+                )
                 break
             else:
                 # case of incorrect response
@@ -60,5 +62,5 @@ def main():
         sock.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- A00/server.py
+++ A00/server.py
@@ -2,9 +2,9 @@ import socket
 
 # global DB for email -> person
 NAME_DB = {
-    'rey.sky@gmail.com': 'Rey Skywalker',
-    'zhenwu@cs.stonybrook.edu': 'ZhengYu Wu',
-    'test1@test.com': 'Test 1'
+    "rey.sky@gmail.com": "Rey Skywalker",
+    "zhenwu@cs.stonybrook.edu": "ZhengYu Wu",
+    "test1@test.com": "Test 1",
 }
 
 
@@ -23,9 +23,9 @@ class Response:
         convert response data into bytes
         :return: bytes for response content
         """
-        type_bytes = bytearray(str.encode(self.r_type, 'utf-8'))
-        length_bytes = bytearray(self.length.to_bytes(1, 'big'))
-        msg_bytes = bytearray(str.encode(self.msg, 'utf-8'))
+        type_bytes = bytearray(str.encode(self.r_type, "utf-8"))
+        length_bytes = bytearray(self.length.to_bytes(1, "big"))
+        msg_bytes = bytearray(str.encode(self.msg, "utf-8"))
         return type_bytes + length_bytes + msg_bytes
 
 
@@ -52,41 +52,43 @@ def main():
     while True:
         # accept connections from outside
         clientsocket, address = serversocket.accept()
-        print('Connected by ', address)
+        print("Connected by ", address)
         # now do something with the clientsocket
         while clientsocket:
             # read data send by client
             # read query type
             q_type_data = clientsocket.recv(1)
-            q_type = q_type_data.decode('utf-8')
-            if q_type == 'Q':
+            q_type = q_type_data.decode("utf-8")
+            if q_type == "Q":
                 # read msg length
                 msg_length_data = clientsocket.recv(1)
                 # decode the msg_length with Big Endian (network byte order)
-                msg_length = int.from_bytes(msg_length_data, 'big')
+                msg_length = int.from_bytes(msg_length_data, "big")
                 # read the actual msg
                 msg_data = clientsocket.recv(msg_length)
-                msg = msg_data.decode('utf-8')
+                msg = msg_data.decode("utf-8")
                 # debug print
-                print(f'------------Question------------\n'
-                      f'Type: {q_type}\nLength: {msg_length}\nQuestion: {msg}\n'
-                      f'--------------------------------')
+                print(
+                    f"------------Question------------\n"
+                    f"Type: {q_type}\nLength: {msg_length}\nQuestion: {msg}\n"
+                    f"--------------------------------"
+                )
                 # get name by email
                 # catching email not in DB
                 try:
                     name = get_name(msg)
                 except KeyError:
-                    name = 'Error Name Not Found!'
+                    name = "Error Name Not Found!"
                 # create response objet
-                response = Response('R', len(name), name)
+                response = Response("R", len(name), name)
                 # send response data to client
                 clientsocket.sendall(response.get_data())
 
             else:
                 # case of incorrect formatting or EOF for connection close
-                clientsocket.sendall(str.encode('Error', 'utf-8'))
+                clientsocket.sendall(str.encode("Error", "utf-8"))
                 break
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
