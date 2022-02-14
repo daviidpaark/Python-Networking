dd3dbd77897aa0d46d2a0057366d458c52bb0339
David Park <daviidpaark@gmail.com>
2022-02-14 15:57:18 -0500

    Handle timeouts

--- A01/mydig.py
+++ A01/mydig.py
@@ -1,3 +1,7 @@
+# David Park
+# dappark
+# 109582425
+
 import sys
 import time
 import datetime
@@ -22,21 +26,24 @@ rootServers = (
 )
 
 
+def queryTimeout():
+    sys.exit("Domain not found/Response timed out")
+
+
 def query(domain):
     # Create DNS query
     question = dns.message.make_query(domain, "A")
     # Send a request to root server
-    answer = dns.query.udp(question, rootServers[0])
+    answer = dns.query.udp(question, rootServers[0], 2)
     # Iterate through DNS until AA is reached
     while not answer.sections[1]:
         # If ADDITIONAL section is empty, query the domain of the nameserver
         if not answer.sections[3]:
-            answer = dns.query.udp(
-                question,
-                dns.name.from_text(
-                    str(query(str(answer.sections[2][0][0])).sections[1][0][0])
-                ),
-            )
+            nameServer = dns.name.from_text(str(answer.sections[2][0][0]))
+            # Make a recursive call to get the IP
+            nameServerIP = query(nameServer)
+            nameServerIP = str(nameServerIP.sections[1][0][0])
+            answer = dns.query.udp(question, nameServerIP)
         # Else, use the IPv4 address in ADDITIONAL
         else:
             for x in answer.sections[3]:
@@ -44,7 +51,7 @@ def query(domain):
                 if x.rdtype == 1:
                     answer = dns.query.udp(question, str(x[0]))
                     break
-    # Handle CDN redirection and query the CNAME
+    # If CNAME is given, handle CDN redirection and query the CNAME
     if answer.sections[1][0].rdtype == 5:
         answer = query(dns.name.from_text(str(answer.sections[1][0][0])))
     return answer
@@ -58,7 +65,11 @@ currentTime = datetime.datetime.now()
 start = time.perf_counter()
 
 # Query the domain
-answer = query(domain)
+try:
+    answer = query(domain)
+except:
+    queryTimeout()
+
 answer = answer.sections[1][0]
 
 end = time.perf_counter()
